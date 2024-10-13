9d38b2b40e1cd4717bf05bf9990c2a2770df7766
Doyun Kim <doyunkm@gmail.com>
2024-10-13 22:24:36 +0900

    implement yahtzee simple and ppo

---
+++ run_ppo_yahtzee_simple.sh
@@ -0,0 +1,4 @@
+
+nohup python ppo_yahtzee_simple.py --exp-name ppo_entcoeff0p1 --ent-coef 0.1 &
+nohup python ppo_yahtzee_simple.py --exp-name ppo_clipcoef0p5 --clip-coef 0.5 &
+nohup python ppo_yahtzee_simple.py --exp-name ppo_lr1p0e-4 --learning-rate 0.0001 &
